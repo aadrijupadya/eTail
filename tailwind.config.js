fa8f0867523c53283ba98cdef02225ff52af6f7e
akhil <akhilvemuri26@gmail.com>
2023-01-26 17:02:43 -0800

    Added Tailwind, Changed formatting on index

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./cart.html",
+    "./checkout.html",
+    "./contact.html",
+    "./detail.html",
+    "./shop.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
