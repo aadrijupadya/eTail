3bd2f27b8dacb0aed68087ce5df3fa90594cff40
Aadrij Upadya <adrij2005@gmail.com>
2023-01-31 00:08:59 -0800

    Signup changes

--- js/login2.js
+++ js/login2.js
@@ -1,9 +1,10 @@
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    var first = document.getElementById("first").value;
     if (username == "akhilvemuri26@gmail.com" && password == "pass") {
-        window.location.replace("index.html");
-        alert("login succesfully");
+        alert("Logged in succesfully, Welcome to Etail, " + first + "!");
+        return false;
 
     } else {
         alert("login failed");
